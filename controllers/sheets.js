9a47f2b0bb7a9212df9bd6fe79e17d126ad65a3e
Michael McBride <michaelmcbride113@gmail.com>
2025-03-18 14:43:09 -0400

    Defined update route

--- controllers/sheets.js
+++ controllers/sheets.js
@@ -10,6 +10,26 @@ router.get("/:sheetId", verifyToken, async (req, res) => {
     } catch {
         res.status(500).json({ err: err.message })
     }
-})
+});
+
+router.put("/:sheetId", verifyToken, async (req, res) => {
+    try {
+        const sheet = await Sheet.findById(req.params.sheetId);
+
+        if (!sheet.author.equals(req.user._id)) {
+            return res.status(403).send("You are not allowed to do that!");
+        }
+
+        const updatedSheet = await Sheet.findByIdAndUpdate(
+            req.params.sheetId,
+            req.body,
+            { new: true }
+        );
+        updatedSheet._doc.author = req.user;
+        res.status(200).json(updatedSheet)
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 module.exports = router;
