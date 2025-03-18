cf8cef33aa2419e06a701ed060ca9adabeaabfdf
Michael McBride <michaelmcbride113@gmail.com>
2025-03-18 15:47:43 -0400

    Removed put route

--- controllers/sheets.js
+++ controllers/sheets.js
@@ -5,42 +5,25 @@ const router = express.Router();
 
 router.get("/:sheetId", verifyToken, async (req, res) => {
     try {
-        const sheet = await Sheet.findById(req.params.sheetId).populate("author");
+        const sheet = await Sheet.findById(req.params.sheetId);
         res.status(200).json(sheet);
     } catch {
         res.status(500).json({ err: err.message })
     }
 });
 
-router.put("/:sheetId", verifyToken, async (req, res) => {
-    try {
-        const sheet = await Sheet.findById(req.params.sheetId);
-
-        if (!sheet.author.equals(req.user._id)) {
-            return res.status(403).send("You are not allowed to do that!");
-        }
-
-        const updatedSheet = await Sheet.findByIdAndUpdate(
-            req.params.sheetId,
-            req.body,
-            { new: true }
-        );
-        updatedSheet._doc.author = req.user;
-        res.status(200).json(updatedSheet)
-    } catch (err) {
-        res.status(500).json({ err: err.message });
-    }
-});
 
 router.delete("/:sheetId", verifyToken, async (req, res) => {
     try {
         const sheet = await Sheet.findById(req.params.sheetId);
-        
-        if (!sheet.author.equals(req.user._id)) {
+        const author = await User.findById(req.body._id);
+
+        if (!author.sheets.includes(sheet._id)) {
             return res.status(403).send("You are not allowed to do that!");
         }
         const deletedSheet = await Sheet.findByIdAndDelete(req.params.sheetId);
         res.status(200).json(deletedSheet);
+
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
