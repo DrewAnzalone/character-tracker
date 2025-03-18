96c53a510164677e75fa626937ec46cbf6468d42
Michael McBride <michaelmcbride113@gmail.com>
2025-03-18 15:54:14 -0400

    Moved code from sheets.js to users.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const logger = require('morgan');
 const authRouter = require('./controllers/auth');
 const testJwtRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
-const sheetsRouter = require('./controllers/sheets.js');
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -29,7 +28,6 @@ app.use(logger('dev'));
 app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
-app.use('/sheets', sheetsRouter)
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
