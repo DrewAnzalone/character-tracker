49d806e83088153a0643bff020cf7053d8c337d6
Michael McBride <michaelmcbride113@gmail.com>
2025-03-18 15:19:29 -0400

    Defined delete route in backend

--- controllers/sheets.js
+++ controllers/sheets.js
@@ -32,4 +32,18 @@ router.put("/:sheetId", verifyToken, async (req, res) => {
     }
 });
 
+router.delete("/:sheetId", verifyToken, async (req, res) => {
+    try {
+        const sheet = await Sheet.findById(req.params.sheetId);
+        
+        if (!sheet.author.equals(req.user._id)) {
+            return res.status(403).send("You are not allowed to do that!");
+        }
+        const deletedSheet = await Sheet.findByIdAndDelete(req.params.sheetId);
+        res.status(200).json(deletedSheet);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
 module.exports = router;
