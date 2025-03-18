93af255a37067bcfac5d12db0d44f0393861b136
Michael McBride <michaelmcbride113@gmail.com>
2025-03-18 14:35:26 -0400

    Defined Show route

--- controllers/sheets.js
+++ controllers/sheets.js
@@ -3,6 +3,13 @@ const verifyToken = require("../middleware/verify-token.js");
 const Sheet = require("../models/sheet.js");
 const router = express.Router();
 
-
+router.get("/:sheetId", verifyToken, async (req, res) => {
+    try {
+        const sheet = await Sheet.findById(req.params.sheetId).populate("author");
+        res.status(200).json(sheet);
+    } catch {
+        res.status(500).json({ err: err.message })
+    }
+})
 
 module.exports = router;
