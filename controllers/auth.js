26844bd8a6c88de45deed2635c7ba6d7a75e9f69
Drew-A <drew.a.anzalone@gmail.com>
2025-03-19 09:21:25 -0700

    added debug route to return user object, whitspace changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const verifyToken = require("../middleware/verify-token.js")
 
 const User = require('../models/user');
 
@@ -10,11 +11,11 @@ const saltRounds = 12;
 router.post('/sign-up', async (req, res) => {
   try {
     const userInDatabase = await User.findOne({ username: req.body.username });
-    
+
     if (userInDatabase) {
-      return res.status(409).json({err: 'Username already taken.'});
+      return res.status(409).json({ err: 'Username already taken.' });
     }
-    
+
     const user = await User.create({
       username: req.body.username,
       hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
@@ -53,5 +54,14 @@ router.post('/sign-in', async (req, res) => {
     res.status(500).json({ err: err.message });
   }
 });
+
+router.get("/debug", verifyToken, async (req, res) => {
+  try {
+    const foundUser = await User.findById(req.user._id).populate("sheets");
+    res.status(200).json(foundUser);
+  } catch (err) {
+    res.status(500).json({ err: "No token found" });
+  }
+})
 
 module.exports = router;
