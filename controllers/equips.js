1f7851fff239cf75f02703b8dce53cc0e9edf24e
Drew-A <drew.a.anzalone@gmail.com>
2025-03-21 08:12:19 -0700

    made equips not delete if equipped to any sheets

--- controllers/equips.js
+++ controllers/equips.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const verifyToken = require("../middleware/verify-token.js")
-const Equip = require("../models/equip.js")
+const express = require("express");
+const verifyToken = require("../middleware/verify-token.js");
+const Equip = require("../models/equip.js");
+const Sheet = require("../models/sheet.js");
 const router = express.Router()
 
 router.post("/", verifyToken, async (req, res) => {
@@ -8,7 +9,7 @@ router.post("/", verifyToken, async (req, res) => {
     const newEquip = await Equip.create(req.body);
     res.status(201).json(newEquip);
   } catch (err) {
-    res.status(500).json({err: err.message});
+    res.status(500).json({ err: err.message });
   }
 });
 
@@ -17,7 +18,7 @@ router.get("/", async (req, res) => {
     const equips = await Equip.find();
     res.status(200).json(equips);
   } catch (err) {
-    res.status(500).json({err: err.message});
+    res.status(500).json({ err: err.message });
   }
 });
 
@@ -32,16 +33,16 @@ router.get('/:equipId', async (req, res) => {
     res.status(200).json(chosenEquip);
   } catch (err) {
     if (res.statusCode === 404) {
-      res.json({err: err.message});
+      res.json({ err: err.message });
     } else {
-      res.status(500).json({err: err.message});
+      res.status(500).json({ err: err.message });
     }
   }
 });
 
 router.put('/:equipId', verifyToken, async (req, res) => {
   try {
-    const updatedEquip = await Equip.findByIdAndUpdate(req.params.equipId, req.body, {new: true});
+    const updatedEquip = await Equip.findByIdAndUpdate(req.params.equipId, req.body, { new: true });
 
     if (!updatedEquip) {
       res.status(404);
@@ -50,27 +51,33 @@ router.put('/:equipId', verifyToken, async (req, res) => {
     res.status(200).json(updatedEquip);
   } catch (err) {
     if (res.statusCode === 404) {
-      res.json({err: err.message});
+      res.json({ err: err.message });
     } else {
-      res.status(500).json({err: err.message});
+      res.status(500).json({ err: err.message });
     }
   }
 });
 
 router.delete('/:equipId', verifyToken, async (req, res) => {
   try {
-    const chosenEquip = await Equip.findByIdAndDelete(req.params.equipId);
-
-    if (!chosenEquip) {
+    const equipToBeDeleted = await Equip.findById(req.params.equipId);
+    if (!equipToBeDeleted) {
       res.status(404);
       throw new Error('Equip not found');
     }
-    res.status(200).json(chosenEquip);
+
+    const isEquipped = !!(await Sheet.findOne({ equips: equipToBeDeleted._id }));
+    if (isEquipped) {
+      throw new Error("Equips on any sheets cannot be deleted");
+    }
+
+    await Equip.findByIdAndDelete(equipToBeDeleted._id);
+    res.status(200).json(equipToBeDeleted);
   } catch (err) {
     if (res.statusCode === 404) {
-      res.json({err: err.message});
+      res.json({ err: err.message });
     } else {
-      res.status(500).json({err: err.message});
+      res.status(500).json({ err: err.message });
     }
   }
 });
